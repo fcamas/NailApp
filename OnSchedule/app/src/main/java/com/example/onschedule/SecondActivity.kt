e3ce416e98bef72153e436c7ae8b21d8c14a35fa
Fredy Camas <fredycamasdev@gmail.com>
2024-02-05 20:25:00 -0500

    added some variables

--- OnSchedule/app/src/main/java/com/example/onschedule/SecondActivity.kt
+++ OnSchedule/app/src/main/java/com/example/onschedule/SecondActivity.kt
@@ -1,9 +1,25 @@
 package com.example.onschedule
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioButton
+import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var mSaveButton: Button
+    private lateinit var mS1: CheckBox
+    private lateinit var mS2: CheckBox
+    private lateinit var mS3: CheckBox
+    private lateinit var mS4: CheckBox
+    private lateinit var mFullName: EditText
+    private lateinit var mEmail: EditText
+    private lateinit var mPhoneNumber: EditText
+    private lateinit var mTime: EditText
+    private lateinit var mRadioam: RadioButton
+    private lateinit var mRadiopm: RadioButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
