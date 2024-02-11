24ffa1d4cefbbccb7d0c0edf77c02ea81e09fd53
Fredy Camas <fredycamasdev@gmail.com>
2024-02-11 16:45:00 -0500

    added nail model

---
+++ OnSchedule/app/src/main/java/com/example/onschedule/Model/NailModel.kt
@@ -0,0 +1,12 @@
+package com.example.onschedule.Model
+
+class NailModel {
+    var id: Int = 0
+    var fullName: String = ""
+    var email: String = ""
+    var phone: String = ""
+    var time: String = ""
+    var service: String = ""
+    var task: String = ""
+    var status: Int = 0
+}
