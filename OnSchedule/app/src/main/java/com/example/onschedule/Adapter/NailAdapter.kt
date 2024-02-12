41d8836e64902803ae4e93d4a38ba98590c9ddea
Fredy Camas <fredycamasdev@gmail.com>
2024-02-12 17:35:00 -0500

    added nail adapter

---
+++ OnSchedule/app/src/main/java/com/example/onschedule/Adapter/NailAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.onschedule.Adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.onschedule.MainActivity
+import com.example.onschedule.Model.NailModel
+
+class NailAdapter( private val mActivity: MainActivity){
+
+    private var mList: List<NailModel> = listOf()
+
+}
