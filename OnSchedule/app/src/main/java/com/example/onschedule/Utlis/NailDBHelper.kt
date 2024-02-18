a7132c0486d78d830df4ffc9dbf117d9ea96eb46
Fredy Camas <fredycamasdev@gmail.com>
2024-02-18 13:45:00 -0500

    added insert task

--- OnSchedule/app/src/main/java/com/example/onschedule/Utlis/NailDBHelper.kt
+++ OnSchedule/app/src/main/java/com/example/onschedule/Utlis/NailDBHelper.kt
@@ -1,4 +1,16 @@
 package com.example.onschedule.Utlis
 
 class NailDBHelper {
+    fun insertTask(nailModel: NailModel) {
+        db = writableDatabase
+        val values = ContentValues()
+        values.put(COL_2, nailModel.fullName)
+        values.put(COL_3, nailModel.email)
+        values.put(COL_4, nailModel.phone)
+        values.put(COL_5, nailModel.time)
+        values.put(COL_6, nailModel.service)
+        values.put(COL_7, nailModel.task)
+        values.put(COL_8, 0)
+        db.insert(TABLE_NAME, null, values)
+    }
 }
